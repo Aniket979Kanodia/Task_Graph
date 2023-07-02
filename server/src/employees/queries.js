098ab6f95cfb8210241ca8284d58f2d887474197
Garvitxx18 <gs2013061@sgsitsindore.in>
2023-07-02 17:10:56 +0530

    Jarvis

--- server/src/employees/queries.js
+++ server/src/employees/queries.js
@@ -1,5 +1,7 @@
 const GetByTeam = "SELECT * FROM employees WHERE team_name=$1";
+const InsertEmployee = "INSERT INTO employees VALUES ($1,$2,$3);";
 
 module.exports = {
   GetByTeam,
+  InsertEmployee,
 };
